218b296d9ea7ed89497fa0837bd0afa29cbd2199
markosestrada42-rgb <markosestrada42@gmail.com>
2026-02-01 18:44:24 -0500

    se actualizo el codigo con funciones render lista y boton eliminar pero aun no se muestra lista productos
    se corrigió la estructura Html y CSS, se creó código en JS, se agrego botón para eliminar en cada producto, Problema pendiente : La lista no se muestra.

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
-let numButtonClicks = 0;
-function buttonClicked() {
-    numButtonClicks = numButtonClicks + 1;
-    document.getElementById("mainDiv").textContent =
-        "Button Clicked times: " + numButtonClicks;
-}
+const input= document.getElementById("productoInput");
+const agregarBton =document.getElementById("agregarBtn");
+const lista =document.getElementById("lista");
+const contador =document.getElementById("contador");
+
+let productos = JSON.parse(localStorage.getItem("productosinput")) || [];
+
+function renderLista(){
+    lista.innerHTML ="";
+    productos.forEach((producto,index) =>{
+    const li = document.createElement("li");
+    li.textContent =producto.nombre;
+     
+    // boton eliminar
+    
+    const eliminarBtn = document.createElement("button");
+    eliminarBtn.textContent = "Eliminar";
+    eliminarbtn.onclick =() => {
+        productos.splice(index,1);
+        guardar();
+        };
+    
+    li.appendChild(eliminarBtn);
+    lista.appendChild (li);
+       
+    });
+    contador.textContent = `Total productos: ${productos.length}`;
+    }
+
+    function guardar(){
+        localStorage.setItem("productosinput", JSON.stringify(producto.length));
+        renderLista();
+    }
+    agregarBtn.onclick = ()=> {
+        const nombre = input.value.trim();
+        if (nombre) {
+            productos.push({nombre});
+            input.value = "";
+            guardar();
+            
+        }
+    };
+    renderLista();
